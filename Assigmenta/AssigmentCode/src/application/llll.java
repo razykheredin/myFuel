686e5121cd00334c28a3fee93b5952aa1e00d4a1
DELL <DELL@DESKTOP-9U4GAUU>
2020-05-26 16:01:18 +0300

    razi

--- Assigmenta/AssigmentCode/src/application/llll.java
+++ Assigmenta/AssigmentCode/src/application/llll.java
@@ -18,7 +18,7 @@ public class llll extends Application {
 	private PasswordField job;
 	@FXML
 	private Button show;
-	
+	///////////////razihalbi
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/application/lll.fxml"));
